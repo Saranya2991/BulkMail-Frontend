7c8272c25cfe5cdd5828a27774ae489de655d8fa
Saranya2991 <saara2991@gmail.com>
2025-12-30 16:24:13 +0000

    updated

--- src/Components/BulkMail.jsx
+++ src/Components/BulkMail.jsx
@@ -67,7 +67,11 @@ function send(){
   }
     console.log("API URL:", import.meta.env.VITE_API_URL);
     setstaus(true)
-    axios.post(import.meta.env.VITE_API_URL+"/sendemail",{subject:subject,msg:msg,emaillist:emaillist})
+    axios.post(import.meta.env.VITE_API_URL+"/sendemail",{subject:subject,msg:msg,emaillist:emaillist},{
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
     .then(function(data){
         if(data.data === true)
         {
